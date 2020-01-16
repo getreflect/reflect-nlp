a0146c3f40580ac611c7ae7c6a2dcb4842776a9f
jackyzha0 <j.zhao2k19@gmail.com>
2020-01-15 22:17:16 -0800

    fixed server attempting to parse form rather than jso n

--- server.py
+++ server.py
@@ -6,12 +6,16 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def parseIntent():
-	intent = request.args.get('intent')
-	print(intent)
+	if request.is_json:
+		intent = request.get_json()["intent"]
 
-	if "yeet" not in intent:
-		return jsonify({'status': 'bad intent'}), 403
-	return jsonify({'status': 'ok'}), 200
+		print(intent)
+
+		if "work" not in intent:
+			return jsonify({'status': 'bad intent'}), 403
+		return jsonify({'status': 'ok'}), 200
+
+	return jsonify({'status': 'bad json'}), 400
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8081)
